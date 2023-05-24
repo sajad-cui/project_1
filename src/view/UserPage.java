0b0224d12cfee573e2d5aeebb9e7543b0ad5c6ba
sajad-cui <sajadshekari1382@gmail.com>
2023-05-24 18:33:58 +0430

    adding user function in view

--- src/view/UserPage.java
+++ src/view/UserPage.java
@@ -56,6 +56,93 @@ public class UserPage {
             System.out.println("[3] : Password : " + buyer.getPassword());
     }
 
-   
+    public static void PrintCart(Products Cart,int i){
+        System.out.println("[" + (i+1) + "] : " +Cart.getClass().getSimpleName()+ " , " + Cart.getName()+" , " + Cart.getProductID() + "if you want to remove this product enter : ["+ -(i+1) + "]" );
+    }
+
+    public static void PrintBuy(){
+        System.out.println("[0] : takmil faraian kharid");
+    }
+
+    public static short scanCart(){ return scan.nextShort(); }
+
+        public static void PrintProduct(Products product){
+            System.out.println("Category : " + product.getCategory());
+            System.out.println("Product Name : " + product.getName());
+            System.out.println("Price : " + product.getPrice());
+            System.out.println("Product ID : " + product.getProductID());
+            System.out.println("Inventory : " + product.getInventory());
+            System.out.println("Average Score : " + Products.getAverageScore());
+
+        }
+
+
+
+
+
+    public static void PrintIncreas(){
+        System.out.print("enter mony to increase credintial : ");
+    }
+    public static double scanIncrement(){ return scan.nextDouble(); }
+
+    public static void PrintCartNumber(){ System.out.print("your cart number :"); }
+
+    public static void PrintPasCart(){ System.out.print("your password cart`s :"); }
+    public static void PrintCVV2(){ System.out.print("your CVV2 :"); }
+
+
+    public static String scanCartNumber(){ return scan.nextLine(); }
+
+    public static String scanPasCart(){ return scan.next(); }
+
+    public static String scanCVV2(){ return scan.next(); }
+
+    public static void PrintWrongcredit(){
+        System.out.println("creditial is wrong  ");
+        System.out.println("[1] : exit ");
+        System.out.println("[2] : increas credit" );
+    }
+    public static short scanWrongcredit(){ return scan.nextShort(); }
+
+
+    public static void PrintFactores(Factore factore,int x) { System.out.println( "["+(x+1)+"]"+"Factore ID : " + factore.getFactoreID()+ " " + "sub product Price : " +factore.getMoney()); }
+    public static short scanFactore(){ return scan.nextShort();}
+
+
+    public static void PrintBuys(Factore factore){
+        for (int i = 0; i < factore.getBuys().size(); i++)
+            System.out.println("["+(i+1)+"]"+factore.getBuys().get(i).getClass().getSimpleName() + ", product name : " + factore.getBuys().get(i).getName() + ", product Price : " + factore.getBuys().get(i).getPrice());
+    }
+    public static short scanBuy(){ return scan.nextShort();}
+
+    public static void printBuy( ){
+        System.out.println("done");
+    }
+
+    public static void printScore( ){
+        System.out.println("[1] : score");
+        System.out.println("[2] : exit");
+    }
+
+    public static short scanScore(){return scan.nextShort(); }
+
+    public static void PrintScore(){ System.out.println("enter the Score"); }
+    public static void scorePrint(){ System.out.println("the Score is updated"); }
+
+
+    public static void PrintText(){
+        System.out.println("Print the Text");
+    }
+
+    public static String scanText(){ return scan.nextLine(); }
+
+    public static void PrintReqCom(){
+        System.out.println("[1] : view");
+        System.out.println("[2] : new Comment");
+    }
+    public static short scanReqCom(){ return scan.nextShort(); }
+
+
+
 
 }
